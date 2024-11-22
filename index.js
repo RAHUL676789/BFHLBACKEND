a54f2d3c6f85d0434463d7b3c6f06f9de390e9a9
=Rahul lodhi <146419777+RAHUL676789@users.noreply.github.com>
2024-11-22 18:46:09 +0530

    origin set

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const bfhlRoutes = require('./Routes/bfhl.js');
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+    origin: 'https://67408044d1097e916070b05a--profound-gingersnap-24916f.netlify.app', // Allow only the frontend domain
+    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+  };
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(bodyParser.json());
