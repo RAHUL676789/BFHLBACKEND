52a620feb88e9c3f150c7c9ff5e3d33e71cc27b2
=Rahul lodhi <146419777+RAHUL676789@users.noreply.github.com>
2024-11-22 19:17:24 +0530

    change

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-    origin: "https://67408044d1097e916070b05a--profound-gingersnap-24916f.netlify.app", // Allow only the frontend domain
-    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
-    allowedHeaders: 'Content-Type,Authorization', // Allowed headers
-  };
+  origin: [
+    "https://67408044d1097e916070b05a--profound-gingersnap-24916f.netlify.app", // Frontend Netlify domain
+  ],
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+app.use(cors(corsOptions));
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); 
 // Middleware
